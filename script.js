dc4e33409e97b109f204f28a5d05465caba2e28b
Alexandre Alves <alexbelloni@outlook.com>
2024-07-28 14:58:42 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,60 @@
+function getProjectCollection() {
+    return [
+        {
+            title: "RESERVATION.LY", banner: "https://res.cloudinary.com/dnzozanco/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1622930668/homepage/reservationly_eegfwg.png",
+            url: "https://reservationly.netlify.app/", hashtags: ["portfolio", "REACT", "REDUX", "GRAPHQL", "NODEJS"],
+            synopsis: "This admin platform is part of an ERP (Enterprise Resource Planning) developed within Hotel Industry. It is a demonstration and a fraction of the original platform.",
+            order: 2,
+        },
+        {
+            title: "CN TOWER LIGHTS", banner: "https://res.cloudinary.com/dnzozanco/image/upload/v1622931305/homepage/cntower_s3n2zd.png",
+            url: "https://cntowerlights.netlify.app/", hashtags: ["portfolio", "REACT", "NODEJS"],
+            synopsis: "CN Tower Lights is a personal project with front-end and back-end layers. It shows what will be the colors of the tower and the reason for that on a specific night. It is not an official website but a tribute for this iconic Toronto landmark.",
+            order: 7,
+        },
+        {
+            title: "DEATH STAR LOGIN", banner: "https://res.cloudinary.com/dnzozanco/image/upload/v1622930369/homepage/death-star_a76mpv.jpg",
+            url: "https://deathstar-web.vercel.app/", hashtags: ["portfolio", "REACT", "NODEJS", "EXPRESS", "JWT", "MONGOOSE", "MONGODB"],
+            synopsis: "CRUD pages to test my auth API in a MERN environment as preview base of a professional project. Reactjs, Node.js, Express, JWT, Mongoose, and MongoDB.",
+            order: 8,
+        },
+        {
+            title: "NASA COMMUNITY BLOG", banner: "https://res.cloudinary.com/dnzozanco/image/upload/v1622930369/homepage/datanauts-projects_xqfkp7.jpg",
+            url: "https://nasadatanauts.github.io/alexbelloni/", hashtags: ["portfolio", "JAVASCRIPT", "blog"],
+            synopsis: "The whole list of projects and stories over my journey in the NASA developer community.",
+            order: 3,
+        },
+        {
+            title: "SHORTLY", banner: "https://res.cloudinary.com/dnzozanco/image/upload/v1622930369/homepage/shortly_iht6wk.jpg",
+            url: "https://yourlinkshortly.netlify.app/", hashtags: ["portfolio", "JAVASCRIPT"],
+            synopsis: "Shortly is a friendly web app, which uses URL shorteners to simplify the sharing of links. It is based on the challenge from the Frontend Mentor website and part of my studies on a Bootcamp program.",
+            order: 6,
+        },
+        {
+            title: "Onde Tem Vacina", banner: "https://res.cloudinary.com/dnzozanco/image/upload/v1622931879/homepage/ondetemvacina_i3urou.png",
+            url: "https://ondetemvacina.com/", hashtags: ["portfolio", "REACT", "blog"],
+            synopsis: "Free collaborative platform that offers a search tool for COVID vaccination places, and allows the population to participate in the process, reporting in real time the situation of the vaccination process in their city. It is my professional contribution as a front-end developer.",
+            order: 0,
+            postId: "ondetemvacina"
+        },
+        {
+            title: "Eat Sleep Ride GPS", banner: "https://res.cloudinary.com/dnzozanco/image/upload/v1622932994/homepage/esr_pro9pn.png",
+            url: "https://eatsleepride.com/", hashtags: ["portfolio", "REACT", "REDUX"],
+            synopsis: "New breed of motorcycle community, which combining the best of today's social web with useful features tailored to motorcycle enthusiasts of all types. It was my professional contribution as a front-end developer for this Canadian startup at DMZ.",
+            order: 1,
+        },
+        // {
+        //     title: "Social Media Press", banner: "https://res.cloudinary.com/dnzozanco/image/upload/w_1000,ar_1:1,c_fill,g_auto/v1654707675/homepage/posts/blog_post_1_jqp9kz.jpg",
+        //     hashtags: ["portfolio", "blog"],
+        //     synopsis: "Collection with some of my external social media posts and articles on the Internet.",
+        //     order: 100,
+        //     postId: "socialmediapress"
+        // },
+        {
+            title: "Countrix", banner: "https://res.cloudinary.com/dnzozanco/image/upload/ar_1:1,c_fill,g_auto,w_1000/v1654783531/homepage/posts/countrix_yb1yni.jpg",
+            url: "https://countrix.netlify.app/", hashtags: ["portfolio", "REACT", "accessibility", "Styled-Components"],
+            synopsis: "It is based on the challenge from the Frontend Mentor website: 'REST Countries API with color theme switcher'. Built as part of my studies on a Bootcamp program. React with routes, Styled-Components, some hooks, and accessibility applied.",
+            order: 4,
+        }
+    ]
+}
