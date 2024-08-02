95eabe3e411d045ad8437b35abdff48c5235bddc
Alexandre Alves <alexbelloni@outlook.com>
2024-08-02 08:35:43 -0400

    Update links and descriptions

--- script.js
+++ script.js
@@ -3,49 +3,49 @@ function getProjectCollection() {
         {
             title: "RESERVATION.LY", banner: "https://res.cloudinary.com/dnzozanco/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1622930668/homepage/reservationly_eegfwg.png",
             url: "https://reservationly.netlify.app/", hashtags: ["portfolio", "REACT", "REDUX", "GRAPHQL", "NODEJS"],
-            synopsis: "This admin platform is part of an ERP (Enterprise Resource Planning) developed within Hotel Industry. It is a demonstration and a fraction of the original platform.",
+            synopsis: "As full-stack developer, I created this product fraction for demonstration is part of an ERP (Enterprise Resource Planning) with a clean and modern design tailored for managing reservations within Hotel Industry.",
             order: 2,
         },
         {
             title: "CN TOWER LIGHTS", banner: "https://res.cloudinary.com/dnzozanco/image/upload/v1622931305/homepage/cntower_s3n2zd.png",
-            url: "https://cntowerlights.netlify.app/", hashtags: ["portfolio", "REACT", "NODEJS"],
-            synopsis: "CN Tower Lights is a personal project with front-end and back-end layers. It shows what will be the colors of the tower and the reason for that on a specific night. It is not an official website but a tribute for this iconic Toronto landmark.",
+            url: "https://github.com/alexbelloni/cntower-lights", hashtags: ["portfolio", "REACT", "NODEJS"],
+            synopsis: "CN Tower Lights is a personal project with both front-end and back-end layers, displaying the CN Tower's colors and their significance for a given night. Though not an official site, it serves as a tribute to this iconic Toronto landmark.",
             order: 7,
         },
         {
             title: "DEATH STAR LOGIN", banner: "https://res.cloudinary.com/dnzozanco/image/upload/v1622930369/homepage/death-star_a76mpv.jpg",
-            url: "https://deathstar-web.vercel.app/", hashtags: ["portfolio", "REACT", "NODEJS", "EXPRESS", "JWT", "MONGOOSE", "MONGODB"],
-            synopsis: "CRUD pages to test my auth API in a MERN environment as preview base of a professional project. Reactjs, Node.js, Express, JWT, Mongoose, and MongoDB.",
+            url: "https://github.com/alexbelloni/deathstar-web", hashtags: ["portfolio", "REACT", "NODEJS", "EXPRESS", "JWT", "MONGOOSE", "MONGODB"],
+            synopsis: "CRUD pages designed to test my authentication API in a MERN stack environment, using React.js, Node.js, Express, JWT, Mongoose, and MongoDB as a preview for a professional project.",
             order: 8,
         },
         {
             title: "NASA COMMUNITY BLOG", banner: "https://res.cloudinary.com/dnzozanco/image/upload/v1622930369/homepage/datanauts-projects_xqfkp7.jpg",
             url: "https://nasadatanauts.github.io/alexbelloni/", hashtags: ["portfolio", "JAVASCRIPT", "blog"],
-            synopsis: "The whole list of projects and stories over my journey in the NASA developer community.",
+            synopsis: "The page features a minimalist and clean design, presenting a professional layout that highlights my role as a Datanaut Open-Data member and Space Apps Challenge Ambassador within a National Aeronautics and Space Administration (NASA) outreach group.",
             order: 3,
         },
         {
             title: "SHORTLY", banner: "https://res.cloudinary.com/dnzozanco/image/upload/v1622930369/homepage/shortly_iht6wk.jpg",
-            url: "https://yourlinkshortly.netlify.app/", hashtags: ["portfolio", "JAVASCRIPT"],
-            synopsis: "Shortly is a friendly web app, which uses URL shorteners to simplify the sharing of links. It is based on the challenge from the Frontend Mentor website and part of my studies on a Bootcamp program.",
+            url: "https://github.com/alexbelloni/shortly", hashtags: ["portfolio", "JAVASCRIPT"],
+            synopsis: "Shortly is a user-friendly web app that simplifies link sharing using URL shorteners. It was developed as part of my Bootcamp studies, based on a Frontend Mentor challenge.",
             order: 6,
         },
         {
-            title: "Onde Tem Vacina", banner: "https://res.cloudinary.com/dnzozanco/image/upload/v1622931879/homepage/ondetemvacina_i3urou.png",
-            url: "https://github.com/alexbelloni/ondetemvacina", hashtags: ["portfolio", "REACT", "blog"],
-            synopsis: "Free collaborative platform that offers a search tool for COVID vaccination places, and allows the population to participate in the process, reporting in real time the situation of the vaccination process in their city. It is my professional contribution as a front-end developer.",
+            title: "Ondetemvacina", banner: "https://res.cloudinary.com/dnzozanco/image/upload/v1622931879/homepage/ondetemvacina_i3urou.png",
+            url: "https://github.com/alexbelloni/ondetemvacina/blob/18104d553e40055020db3ecb3ec950903d235e3f/README.md", hashtags: ["portfolio", "REACT", "blog"],
+            synopsis: "A free collaborative platform mapping COVID vaccination sites and real-time reporting on vaccination progress for Brazilian citizens, developed by our team and me as a front-end developer.",
             order: 0
         },
         {
             title: "Eat Sleep Ride GPS", banner: "https://res.cloudinary.com/dnzozanco/image/upload/v1622932994/homepage/esr_pro9pn.png",
             url: "https://eatsleepride.com/", hashtags: ["portfolio", "REACT", "REDUX"],
-            synopsis: "New breed of motorcycle community, which combining the best of today's social web with useful features tailored to motorcycle enthusiasts of all types. It was my professional contribution as a front-end developer for this Canadian startup at DMZ.",
+            synopsis: "New breed of motorcycle community, which combining the best of today's social web with useful features tailored to motorcycle enthusiasts of all types, created by a tech Canadian startup at DMZ incubator in Toronto, and having my professional contribution as a front-end developer.",
             order: 1
         },
         {
             title: "Countrix", banner: "https://res.cloudinary.com/dnzozanco/image/upload/ar_1:1,c_fill,g_auto,w_1000/v1654783531/homepage/posts/countrix_yb1yni.jpg",
-            url: "https://countrix.netlify.app/", hashtags: ["portfolio", "REACT", "accessibility", "Styled-Components"],
-            synopsis: "It is based on the challenge from the Frontend Mentor website: 'REST Countries API with color theme switcher'. Built as part of my studies on a Bootcamp program. React with routes, Styled-Components, some hooks, and accessibility applied.",
+            url: "https://github.com/alexbelloni/countrix?tab=readme-ov-file", hashtags: ["portfolio", "REACT", "accessibility", "Styled-Components"],
+            synopsis: "This project, based on the Frontend Mentor challenge 'REST Countries API with color theme switcher,' was built during my Bootcamp studies. It features React with routing, Styled-Components, various hooks, and accessibility enhancements.",
             order: 4,
         }
     ]
